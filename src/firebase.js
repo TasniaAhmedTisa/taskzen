cde870a2371f59a5fe363163408fc578c0f33f5e
TasniaAhmedTisa <tasniaahmed055@gmail.com>
2025-02-20 15:32:53 +0600

    firebase.js file add

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA0yRySCh6oKxRisXazQTqzAgYigBtdRQE",
+  authDomain: "taskzen-1fbca.firebaseapp.com",
+  projectId: "taskzen-1fbca",
+  storageBucket: "taskzen-1fbca.firebasestorage.app",
+  messagingSenderId: "98623495634",
+  appId: "1:98623495634:web:a52f143a3c93cc7adb7c0b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
